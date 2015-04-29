9e75f3b14e6004e192af45bc50e2a8036100e8b1
Eric Shen <ericsheng@livenation.com>
2015-04-29 14:49:22 +0800

    add change

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./app/routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -21,7 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', routes.index);
+app.use('/online', routes.online);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +38,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('pages/error', {
+    res.render('app/views/common/error', {
       message: err.message,
       error: err
     });
@@ -48,7 +49,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('pages/error', {
+  res.render('app/views/common/error', {
     message: err.message,
     error: {}
   });
